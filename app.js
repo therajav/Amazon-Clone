228ffc2761c28d99f5462cfe607988261120fcc8
therajav <mobiraja2003@gmail.com>
2024-05-12 21:45:28 +0530

    hero page image slider and JS file created

---
+++ app.js
@@ -0,0 +1,29 @@
+const imgs = document.querySelectorAll('.header-slider ul img');
+const nextButton = document.querySelector('.control-next');
+const prevButton = document.querySelector('.control-prev');
+
+let n = 0;
+
+function changeSlide() {
+    for (let i = 0; i < imgs.length; i++) {
+        imgs[i].style.display = 'none';
+    }
+    imgs[n].style.display = 'block';
+}
+changeSlide();
+prevButton.addEventListener('click', (e) => {
+    if (n > 0) {
+        n--;
+    } else {
+        n = imgs.length - 1;
+    }
+    changeSlide()
+})
+nextButton.addEventListener('click', (e) => {
+    if (n < imgs.length - 1) {
+        n++;
+    } else {
+        n = 0;
+    }
+    changeSlide()
+})
